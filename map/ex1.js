80119f09446bd794a42600ed573b6f5736b63acf
Cyril Carral <cyril.carral85@gmail.com>
2019-10-08 11:17:18 +0200

    map ex1.js done

--- map/ex1.js
+++ map/ex1.js
@@ -21,8 +21,11 @@ En sortie: [
 
  */
 
-function getStringsLength(strings) {
+const getStringsLength = (strings) => {
+  return strings.map((word) => `${word} contains ${word.length} characters`);
 }
+
+console.log(getStringsLength(['Chicken', 'Bacon', 'Tofu', 'Mayonnaise']))
 
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
 // mais alors, il faut alors lancer l'exercice avec node map/ex1
